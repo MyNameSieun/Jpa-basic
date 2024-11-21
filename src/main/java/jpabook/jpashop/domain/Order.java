ec26a6ab5cb61f90cdce2327604e78ca5e57040a
박시은 <charm516@naver.com>
2024-11-21 12:51:15 +0900

    [feat] - Implemented basic entity mapping for JPA

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,17 +1,53 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+// DB에는 ORDER가 예약어로 걸려있기 때문에 보통 ORDERS로 사용
+@Table(name = "ORDERS")
 public class Order {
 
     @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
     private Long id;
 
+    @Column(name = "MEMBER_ID")
     private Long memberId;
     private LocalDateTime orderDate;
     private orderStatus status;
+
+    //getter setter (Alt+Insert)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public orderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(orderStatus status) {
+        this.status = status;
+    }
 }
